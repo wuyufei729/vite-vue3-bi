99aa63558e1496f0281868e43ca9b210cf2ad072
吴玉飞 <5402511+wuyufei729@user.noreply.gitee.com>
2024-05-15 21:12:54 +0800

    update title

--- src/views/home/subModules/homeContainer/subModules/homeContainerRight/subModules/performanceTrends.vue
+++ src/views/home/subModules/homeContainer/subModules/homeContainerRight/subModules/performanceTrends.vue
@@ -1,10 +1,93 @@
+
 <template>
-    <div class="">
-        性能曲线
+    <div class="group-item performance-trends">
+        <h3 class="common-btn-title">性能曲线</h3>
+        
+        <p class="animate-liner-line"></p>
+        <div class="performance-chart-wrap">
+            <dv-charts
+                :option="option"
+                style="width:100%;height:100%;"
+            />
+        </div>
     </div>
-    
+
 </template>
 
 <script lang='ts' setup>
+const option = {
+    title: {
+        show: false,
+    },
+    grid: {
+        left: '10%',
+        right: '10%',
+        top: '4%',
+        bottom: '10%',
+    },
+    xAxis: {
+        axisTick:{
+            show: false,
+        },
+        axisLine:{
+            style: {
+                stroke: '#01DDFE',
+                lineWidth: 1
+            }
+        },
 
+        axisLabel:{
+            style: {
+                fill: '#01DDFE',
+                fontSize: 10,
+                rotate: 0
+            }
+        },
+
+        name: "第二周",
+        data: ["周一", "周二", "周三", "周四", "周五", "周六", "周日"]
+    },
+    yAxis: {
+        name: "负荷压力值",
+        nameTextStyle: {
+            fill: '#01DDFE',
+            fontSize: 10
+        },
+        data: "value",
+        axisTick:{
+            show: false,
+        },
+        axisLine:{
+            style: {
+                stroke: '#01DDFE',
+                lineWidth: 1
+            }
+        },
+        axisLabel:{
+            style: {
+                fill: '#01DDFE',
+                fontSize: 10,
+                rotate: 0
+            }
+        },
+        splitLine:{
+            style: {
+                stroke: '#08687C',
+                lineWidth: 1
+            }
+        }
+    },
+    series: [
+        {
+            smooth: true,
+            data: [1200, 4230, 3900, 2100, 1500, 4200, 3985],
+            type: "line",
+            lineArea: {
+                show: true,
+                gradient: ["rgba(55, 162, 218, 0.6)", "rgba(55, 162, 218, 0)"]
+            }
+        }
+    ]
+};
 </script>
+
--- src/views/home/subModules/homeHeader/homeHeader.vue
+++ src/views/home/subModules/homeHeader/homeHeader.vue
@@ -8,6 +8,12 @@
             <!-- <dv-decoration8 :reverse="true" style="width:300px;height:50px;" />
             空压机监控系统
             <dv-decoration8 style="width:300px;height:50px;" /> -->
+            
+            <dv-decoration-11 style="width:100%;height:80px;opacity:1">
+                <h1>空压机监控系统</h1>
+            </dv-decoration-11>
+            
+            
         </div>
         <div class="right">
             <label>当前登录人：张兴路</label>
